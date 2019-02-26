3964250d566d53dcab66ff22b6ef23e11838edca
fanniyuliani08 <fanni.yuliani08@gmail.com>
2019-02-26 12:11:37 +0700

    selesai

---
+++ app/src/main/java/smktelkom/www/hitungpersegi/MainActivity.java
@@ -0,0 +1,61 @@
+package smktelkom.www.hitungpersegi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editSisi, edithasil, editkeliling;
+    private Button btnOk;
+    private TextView txtsisi, txtluas, txtkeliling;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initUI();
+        initEvent();
+    }
+
+    private void initUI() {
+        txtsisi = findViewById(R.id.txtSisi);
+        txtluas = findViewById(R.id.txtluas);
+        txtkeliling = findViewById(R.id.txtKeliling);
+        editSisi = findViewById(R.id.editSisi);
+        edithasil = findViewById(R.id.editHasil);
+        editkeliling = findViewById(R.id.editKeliling);
+        btnOk = findViewById(R.id.btnOKHitung);
+
+    }
+
+    private void initEvent() {
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hitungLuas();
+                hitungKeliling();
+
+            }
+        });
+    }
+
+    private void hitungLuas() {
+        int sisi = Integer.parseInt(editSisi.getText().toString());
+        int luas = sisi * sisi;
+        edithasil.setText(luas + "");
+
+    }
+
+    private void hitungKeliling() {
+        int sisi = Integer.parseInt(editSisi.getText().toString());
+        int keliling = 4 * sisi;
+        editkeliling.setText(keliling + "");
+    }
+}
+
+
